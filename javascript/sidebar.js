02721e01b84b5c6eedd03311d37b2f55e26831bb
Fermazate <fernandomazate@gmail.com>
2024-02-15 09:26:05 -0600

    blurring content when sidebar is open

--- javascript/sidebar.js
+++ javascript/sidebar.js
@@ -3,6 +3,7 @@ const sidebar = document.querySelector(".sidebar");
 const sidebarOpenBtn = document.querySelector("#sidebar-open");
 const sidebarCloseBtn = document.querySelector("#sidebar-close");
 const sidebarLockBtn = document.querySelector("#lock-icon");
+const content=document.querySelector("#shade")
 
 //toogle the lock state of the sidebar
 
@@ -21,17 +22,24 @@ const toggleLock = () =>{
 const hideSidebar= () => {
     if(sidebar.classList.contains("unlocked")) {
         sidebar.classList.add("close");
+        content.classList.remove("blurred");
     }
 };
 
 const showSidebar= () => {
     if(sidebar.classList.contains("unlocked")){
         sidebar.classList.remove("close");
+        content.classList.add("blurred");
     }
 }
 
 const toggleSidebar = () => {
     sidebar.classList.toggle("close");
+    if(sidebar.classList.contains("close")){
+        content.classList.remove("blurred");
+    } else {
+        content.classList.add("blurred");
+    }
 };
 
 if (window.innerWidth<800){
